4eed929f5adf5c1106beda781bf111a1c6ca1616
Alezml <aka76aka@gmail.com>
2021-04-20 10:28:08 +0300

    migration

---
+++ YORMUNGAND/Migrations/20210420072625_d47.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace YORMUNGAND.Migrations
+{
+    public partial class d47 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Blink",
+                table: "RPAAlert",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "Blink",
+                table: "RPAAlert",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
